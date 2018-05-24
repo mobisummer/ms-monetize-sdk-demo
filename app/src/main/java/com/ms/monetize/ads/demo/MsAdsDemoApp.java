fca19a88928c038dca468858b09c1fb1e94a8181
alexgu <304704787@qq.com>
2018-05-24 17:49:09 +0800

    2.1.9

--- app/src/main/java/com/ms/monetize/ads/demo/MsAdsDemoApp.java
+++ app/src/main/java/com/ms/monetize/ads/demo/MsAdsDemoApp.java
@@ -1,8 +1,11 @@
 package com.ms.monetize.ads.demo;
 
 import android.app.Application;
+import android.widget.Toast;
 
+import com.ms.monetize.ads.AdError;
 import com.ms.monetize.ads.MsAdsSdk;
+import com.ms.monetize.ads.StartupListener;
 
 
 public class MsAdsDemoApp extends Application {
@@ -12,7 +15,17 @@ public class MsAdsDemoApp extends Application {
   public void onCreate() {
     super.onCreate();
 
-    MsAdsSdk.start(this, "ms2.1.0@monetize");
+    MsAdsSdk.start(this, "ms2.1.0@monetize", new StartupListener() {
+      @Override
+      public void onSuccess() {
+        Toast.makeText(MsAdsDemoApp.this,"success",Toast.LENGTH_LONG).show();
+      }
+
+      @Override
+      public void onFailed(AdError adError) {
+        Toast.makeText(MsAdsDemoApp.this,"failed",Toast.LENGTH_LONG).show();
+      }
+    });
   }
 
 
